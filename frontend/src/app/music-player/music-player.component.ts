d41c7184c5ec17151d25e57ec7abda390f10d59b
Mindaugas <budr.mindaugas@gmail.com>
2018-10-29 16:56:53 +0100

    Some player functionality added

--- frontend/src/app/music-player/music-player.component.ts
+++ frontend/src/app/music-player/music-player.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MusicPlayerComponent implements OnInit {
 
+  public audio = new Audio();
+  public currentlyPlaying : boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.audio.src = "http://localhost:3000/api/file/download?filename=wutang1.mp3";
+    this.audio.load();
+    this.audio.volume = 0.2;
   }
 
+  public pressedButton(){
+    if(this.currentlyPlaying){
+      this.pauseAudio();
+      console.log("should be paused now.")
+
+    }
+    else{
+      this.playAudio();
+      console.log("should start playing now.")
+    }
+  }
+
+  public pauseAudio(){
+    this.audio.pause();
+    this.currentlyPlaying = false;
+  }
+
+  public playAudio(){
+
+    this.audio.play();
+    this.currentlyPlaying = true;
+
+  }
 }
