cf8f39544cf7034c7c3d18cd95c050681d7c2fa6
Mindaugas <budr.mindaugas@gmail.com>
2018-11-25 17:10:53 +0100

    can properly upload an album now with folder name + album id in parameters.

--- backend/app.js
+++ backend/app.js
@@ -12,9 +12,6 @@ var requestLOL = require('request');
 
 var gridfs = require('gridfs-stream');
 
-
-
-
 //for file tag reading
 const NodeID3 = require('node-id3');
 
@@ -30,11 +27,11 @@ var albumRouter = require('./routes/album');
 var genreRouter = require('./routes/genre');
 
 var app = express();
- 
-// var dev_db_url = 'mongodb://holysmokes:kasekas1212@ds155352.mlab.com:55352/smokifydb';
-// mongoose.connect(dev_db_url);
 
-mongoose.connect('mongodb://localhost:27017/mean-angular6');
+var dev_db_url = 'mongodb://holysmokes:kasekas1212@ds155352.mlab.com:55352/smokifydb';
+mongoose.connect(dev_db_url);
+
+// mongoose.connect('mongodb://localhost:27017/mean-angular6');
 
 
 // mongodb://<dbuser>:<dbpassword>@ds155352.mlab.com:55352/smokifydbZzZ
@@ -58,123 +55,93 @@ connection.once('open', () => {
   var gfs = gridfs(connection.db);
 
   app.get('/', (req, res) => {
-      res.send('Download/Upload GridFS files to MongoDB <br>- by JavaSampleApproach.com');
+    res.send('Download/Upload GridFS files to MongoDB <br>- by JavaSampleApproach.com');
   });
 
   // Upload a file from loca file-system to MongoDB
   app.get('/api/file/upload', (req, res) => {
-  
-  var filename = req.query.filename;
-  
-      var writestream = gfs.createWriteStream({ filename: filename });
-      fs.createReadStream(__dirname + "/public" + filename).pipe(writestream);
-      writestream.on('close', (file) => {
-          res.send('Stored File: ' + file.filename + ' , id is: ' + file._id);
-      });
+
+    var filename = req.query.filename;
+
+    var writestream = gfs.createWriteStream({ filename: filename });
+    fs.createReadStream(__dirname + "/public" + filename).pipe(writestream);
+    writestream.on('close', (file) => {
+      res.send('Stored File: ' + file.filename + ' , id is: ' + file._id);
+    });
   });
 
-    //---------------------------
-    // SINGLE FILE UPLOAD WITH ADDITION TO DB AS SONG
-
-    //db nebe single, o visa folder.
-    app.get('/api/file/uploadsong', (req, res) => {
-  
-
-      // var albumID = "5bf451544a7c2026d063b0be";
-
-      var albumID = req.query.albumID;
-
-      
-      
-      var fileList = [];
-      fs.readdir(__dirname + "/public/test4", (err, files) => {
-
-        fileList = files;
-        console.log('FILELIST:             ------')
-        console.log('file list ilgis: ' + fileList.length)
-        console.log(fileList);
+  //full album/folder addition to mongo
+  app.get('/api/file/uploadalbum', (req, res) => {
 
 
-        console.log('LOOPAS VEIKIA.')
-        for(var i = 0; i < fileList.length; i++){
-  
-          let filepath = (__dirname + "/public/test4/" + fileList[i]);
-          let tags = NodeID3.read(filepath);
-          console.log('TAGS nureadinom, title + trackNumber: -----------------------------')
-          console.log(tags.title + "   --   " + tags.trackNumber);
-  
-          // mp3Duration(filepath, function (err, duration) {
-          //   if (err) return console.log(err.message);
-          //   // console.log(fileList)
-          //   console.log(fileList[3] + ' is: ' + duration + ' seconds long');
-          // });
+    // var albumID = "5bf451544a7c2026d063b0be";
 
-          var savedFileID = '';
-          var writestream = gfs.createWriteStream({ filename: fileList[i] });
-          fs.createReadStream(__dirname + "/public/test4/" + fileList[i]).pipe(writestream);
-          writestream.on('close', (file) => {
-              savedFileID = file._id;
-              console.log('SAVEDFILEID: ');
-              console.log(savedFileID);
-              // res.send('Stored File: ' + file.filename + ' , id is: ' + file._id);
-  
-  
-              var myJSONObject = {
-                title: tags.title,
-                track_number: tags.trackNumber,
-                album: albumID,
-                song_file: savedFileID
-                // duration: ''
-               };
-  
-              requestLOL({
-                  url: "http://localhost:3000/api/songs",
-                  method: "POST",
-                  json: true,   // <--Very important!!!
-                  body: myJSONObject
-              }, function (error, response, body){
-                console.log('BANDOM POST IDET, cia json: --------------------------------')
-                console.log(myJSONObject)
-                // console.log(response);
-              });
+    var albumID = req.query.albumID;
+    var folderName = req.query.folderName;
+    
+    console.log(albumID);
+    console.log(folderName);
+
+    var fileList = [];
+    fs.readdir(__dirname + "/public/" + folderName + '/', (err, files) => {
+
+      fileList = files;
+      console.log('FILELIST:             ------')
+      console.log('file list ilgis: ' + fileList.length)
+      console.log(fileList);
+
+
+      console.log('LOOPAS VEIKIA.')
+      for (var i = 0; i < fileList.length; i++) {
+
+        let filepath = (__dirname + "/public/" + folderName + '/' + fileList[i]);
+        console.log(filepath);
+        let tags = NodeID3.read(filepath);
+        console.log('TAGS nureadinom, title + trackNumber: -----------------------------')
+        console.log(tags.title + "   --   " + tags.trackNumber);
+
+        // mp3Duration(filepath, function (err, duration) {
+        //   if (err) return console.log(err.message);
+        //   // console.log(fileList)
+        //   console.log(fileList[3] + ' is: ' + duration + ' seconds long');
+        // });
+
+        var savedFileID = '';
+        var writestream = gfs.createWriteStream({ filename: fileList[i] });
+        fs.createReadStream(__dirname + "/public/" + folderName + '/' + fileList[i]).pipe(writestream);
+        writestream.on('close', (file) => {
+          savedFileID = file._id;
+          console.log('SAVEDFILEID: ');
+          console.log(savedFileID);
+          // res.send('Stored File: ' + file.filename + ' , id is: ' + file._id);
+
+
+          var myJSONObject = {
+            title: tags.title,
+            track_number: tags.trackNumber,
+            album: albumID,
+            song_file: savedFileID
+            // duration: ''
+          };
+
+          requestLOL({
+            url: "http://localhost:3000/api/songs",
+            method: "POST",
+            json: true,   // <--Very important!!!
+            body: myJSONObject
+          }, function (error, response, body) {
+            console.log('BANDOM POST IDET, cia json: --------------------------------')
+            console.log(myJSONObject)
+            // console.log(response);
           });
-
-
-
-          
-        }
-  
-  
-
-  
-
-
-
-
-
-
         });
 
+      }
+
+    });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      });
+  });
 
 
 
@@ -184,9 +151,9 @@ connection.once('open', () => {
   // //TEST FOLDER ---------------------------------------
   // //upload folder
   // app.get('/api/file/uploadfolder', (req, res) => {
-  
+
   //   var filename = req.query.filename;
-    
+
   //       var writestream = gfs.createWriteStream({});
   //       fs.createReadStream(__dirname + "/public/" + filename).pipe(writestream);
   //       writestream.on('close', (file) => {
@@ -198,68 +165,68 @@ connection.once('open', () => {
   // Download a file from MongoDB - then save to local file-system
   // streams/downloads file by object id
   app.get('/api/file/download', (req, res) => {
-  // Check file exist on MongoDB
-  
-  var filename = req.query.filename;
-  var objectID = req.query.objectID;
-  
-      gfs.exist({ _id: objectID}, (err, file) => {
-          if (err || !file) {
-              res.status(404).send('File Not Found');
-      return
-          } 
-    
-    res.header('Content-Type', 'audio/mpeg',);
+    // Check file exist on MongoDB
 
-    //TO DO:
-    // 1. content-length idet del duration? gal kazka pafixins pvz chrome?
-    // 2. https://developer.mozilla.org/en-US/docs/Web/HTTP/Configuring_servers_for_Ogg_media
+    var filename = req.query.filename;
+    var objectID = req.query.objectID;
 
+    gfs.exist({ _id: objectID }, (err, file) => {
+      if (err || !file) {
+        res.status(404).send('File Not Found');
+        return
+      }
 
+      res.header('Content-Type', 'audio/mpeg');
+
+      //TO DO:
+      // 1. content-length idet del duration? gal kazka pafixins pvz chrome?
+      // 2. https://developer.mozilla.org/en-US/docs/Web/HTTP/Configuring_servers_for_Ogg_media
 
 
 
 
-    // res.header('Content-Length', );
-    // console.log(file.status);
-    // console.log(file.);
 
-    var readstream = gfs.createReadStream({_id: objectID});
-    // res.set('Content-Type', 'audio/mpeg');
-    readstream.pipe(res);
-      });
+
+      // res.header('Content-Length', );
+      // console.log(file.status);
+      // console.log(file.);
+
+      var readstream = gfs.createReadStream({ _id: objectID });
+      // res.set('Content-Type', 'audio/mpeg');
+      readstream.pipe(res);
+    });
   });
 
   // Delete a file from MongoDB
   app.get('/api/file/delete', (req, res) => {
-  
-  var filename = req.query.filename;
-  
-  gfs.exist({ filename: filename }, (err, file) => {
-    if (err || !file) {
-      res.status(404).send('File Not Found');
-      return;
-    }
-    
-    gfs.remove({ filename: filename }, (err) => {
-      if (err) res.status(500).send(err);
-      res.send('File Deleted');
+
+    var filename = req.query.filename;
+
+    gfs.exist({ filename: filename }, (err, file) => {
+      if (err || !file) {
+        res.status(404).send('File Not Found');
+        return;
+      }
+
+      gfs.remove({ filename: filename }, (err) => {
+        if (err) res.status(500).send(err);
+        res.send('File Deleted');
+      });
     });
-  });
   });
 
   // Get file information(File Meta Data) from MongoDB
   app.get('/api/file/meta', (req, res) => {
-    
+
     var filename = req.query.filename;
-    
+
     gfs.exist({ filename: filename }, (err, file) => {
       if (err || !file) {
         res.send('File Not Found');
         return;
       }
-      
-      gfs.files.find({ filename: filename }).toArray( (err, files) => {
+
+      gfs.files.find({ filename: filename }).toArray((err, files) => {
         if (err) res.send(err);
         res.json(files);
       });
@@ -294,7 +261,7 @@ app.use('/api', genreRouter);
 
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
